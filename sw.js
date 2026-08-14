6bcd3bfe33635068614dfbdef803db9b3426963c
ibrahemhelmy2323-hue <ibrahem.helmy2323@gmail.com>
2026-08-15 02:40:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'thrive-v1';
+const ASSETS = [
+  './index.html',
+  './logo.svg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS).catch(err => console.log("Caching asset failed", err));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
